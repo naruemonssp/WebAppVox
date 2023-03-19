2915331abce3d4669d14f84771ecb19d6977b68d
sitthatkaja <sitthatkaja@kkumail.com>
2023-03-19 21:18:55 +0700

    web app

---
+++ server_0.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template, request, send_file
+from PIL import Image
+import subprocess
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('test_upload.html') #แสดงผลหน้าหลัก
+
+@app.route('/upload', methods=['POST'])
+def upload():
+    file = request.files['image']
+    image = Image.open(file)
+    processed_image.save('static/jpg test.jpg')
+    return render_template('upload_completed.html') #ไปหน้า complete
+    
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
